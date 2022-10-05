74e11494f564c6e5f31d21de5e4cfc5513735c91
ron <@>
2022-10-05 18:44:54 +0300

    Tests Passing: DAD, RLC

--- tests/test_dad.c
+++ tests/test_dad.c
@@ -1,16 +1,27 @@
 #include <stdlib.h>
 #include "../src/instructions.h"
 #include "../src/instructions.c"
-#include "../src/cpu.h"
 #include "../src/cpu.c"
 
 int main (void)
 {
+	int passed = 0;
 	cpu c;
 	cpu_init (&c);
 
-	uint8_t *memory = calloc (10000, 1);
-	cpu_set_memory (&c, memory);
+	cpu_set_bc (&c, 0x339f);
+	cpu_set_hl (&c, 0xa17b);
 
-	return 0;
+	dad_b (&c);
+
+	if (cpu_get_hl (&c) == 0xd51a && !c.flag_c) { passed++; }
+
+	cpu_set_hl (&c, 0x04f2);
+	dad_h (&c);
+
+	if (cpu_get_hl (&c) == 0x09e4 && !c.flag_c) { passed++; }
+
+	if (passed == 2) { return 0; }
+
+	return 1;
 }
--- tests/test_rlc.c
+++ tests/test_rlc.c
@@ -7,5 +7,10 @@ int main (void)
 	cpu c;
 	cpu_init (&c);
 
+	c.a = 0Xf2;
+	rlc (&c);
+
+	if (c.a == 0xe5 && c.flag_c) { return 0; }
+
 	return 1;
 }
