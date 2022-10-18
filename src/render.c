f49ad17ac292a75ca022f0a2d889f2a061199cf6
ron <@>
2022-10-18 15:15:55 +0300

    Removed: R_CompletePixel - Unused function

--- src/render.c
+++ src/render.c
@@ -44,14 +44,6 @@ void R_ClearScreen (SDL_Renderer **ren)
 	SDL_RenderClear (*ren);
 }
 
-void R_CompletePixel (SDL_Renderer **ren, int r, int g, int b, int x, int y)
-{
-	// if (r > 0xff || g > 0xff || b > 0xff) { R_LogWarning ("Invalid pixel RGB values"); }
-
-	SDL_SetRenderDrawColor (*ren, r & 0xff, g & 0xff, b & 0xff, 0xff);
-	SDL_RenderDrawPoint (*ren, x, y);
-}
-
 void R_SetColorBlack (SDL_Renderer **ren) { SDL_SetRenderDrawColor (*ren, 0, 0, 0, 0xff); }
 void R_SetColorWhite (SDL_Renderer **ren) { SDL_SetRenderDrawColor (*ren, 0xff, 0xff, 0xff, 0xff); }
 
