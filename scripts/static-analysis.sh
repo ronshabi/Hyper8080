c86e30bcd10645809a396dd506f24ccd9971a099
Ron Shabi <ron@ronsh.net>
2022-12-01 00:32:20 +0200

    Skeleton for static-analysis

--- scripts/static-analysis.sh
+++ scripts/static-analysis.sh
@@ -1,5 +1,20 @@
+#!/bin/sh
+
 # Copyright 2022(c) Ron Shabi
 
 # This script invokes the Clang static analyzer (aka scan-build)
 
+# check if not running in root dir
 
+pwd=$(pwd)
+
+
+# Check if script is running from 'scripts' directory.
+# This is a light way of preventing misuse of the script.
+# FIXME: Add more run-path preventions to this script
+if [[ ${pwd} =~ "scripts" ]]; then
+    echo "Please run me from the root directory"
+    exit 1
+fi
+
+# Normal execution path
