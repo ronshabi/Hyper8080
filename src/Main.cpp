33acb6935b64a5aa7f847310fd1f7e2f89807722
Ron Shabi <ron@ronsh.net>
2023-07-26 23:06:24 +0300

    added: C++ main function

---
+++ src/Main.cpp
@@ -0,0 +1,42 @@
+#include <Emulation/CPU.hpp>
+
+#include <GUI/Backend.hpp>
+#include <GUI/Window.hpp>
+
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
+
+
+#include <iostream>
+
+void PrintUsage() {
+    std::cerr << "usage: Hyper8080 <file>\n";
+}
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 2) {
+        PrintUsage();
+        std::exit(1);
+    }
+
+    std::string programPath(argv[1]);
+    spdlog::set_level(spdlog::level::debug);
+
+    // Load program
+    CPU cpu;
+
+    try {
+        cpu.LoadProgram(programPath);
+    } catch (const std::runtime_error& rte) {
+        spdlog::error("{}", rte.what());
+        std::exit(1);
+    }
+
+    // Initialize backend
+    GUI::Backend backend;
+    GUI::Window mainWindow("Hyper8080", 2);
+
+    mainWindow.SetPixelBuffer(cpu.GetMemoryAtOffset(0), 1000);
+
+}
